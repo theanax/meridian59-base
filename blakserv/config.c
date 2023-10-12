a2125a1019786e4cf90904cd3d22e4de7e8baecf
theCornballer <119654160+theCornballer@users.noreply.github.com>
2023-10-12 12:41:54 -0500

    Change default bios

--- blakserv/config.c
+++ blakserv/config.c
@@ -181,9 +181,9 @@ config_table_type config_table[] =
    
 { ADVERTISE_GROUP,        F, "[Advertise]",   CONFIG_GROUP, "" },
 { ADVERTISE_FILE1,        T, "File1",         CONFIG_STR,   "ad1.avi" },
-{ ADVERTISE_URL1,         T, "Url1",          CONFIG_STR,   "https://www.meridiannext.com/" },
+{ ADVERTISE_URL1,         T, "Url1",          CONFIG_STR,   "https://www.m59eternals.com/" },
 { ADVERTISE_FILE2,        T, "File2",         CONFIG_STR,   "ad2.avi" },
-{ ADVERTISE_URL2,         T, "Url2",          CONFIG_STR,   "https://www.meridiannext.com/phpbb3/" },
+{ ADVERTISE_URL2,         T, "Url2",          CONFIG_STR,   "https://www.m59eternals.com/" },
 
 { DEBUG_GROUP,            F, "[Debug]",       CONFIG_GROUP, "" },
 { DEBUG_HEAP,             T, "Heap",          CONFIG_BOOL,  "No" },
